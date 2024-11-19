2ce072ba3040fd68ff5c5354021198387327eb22
matheus <mathe@DESKTOP-D2RBRI4>
2024-11-18 23:55:18 -0300

    Initial UI configuration

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { SidebarModule } from './sidebar/sidebar.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, SidebarModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/sidebar/sidebar.module.ts
@@ -0,0 +1,11 @@
+import { SidebarComponent } from "./sidebar.component";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [SidebarComponent],
+    exports: [SidebarComponent],
+    providers: [],
+    bootstrap: []
+})
+
+export class SidebarModule {}

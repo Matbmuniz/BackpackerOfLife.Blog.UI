f8a6c6a1890107698f207b80fc4afa14705ce4f8
matheus <mathe@DESKTOP-D2RBRI4>
2024-12-05 00:37:43 -0300

    adjust-performanceSidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,18 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule, MatSidenavModule, MatButtonModule],
+  imports: [MatSidenavModule, MatButtonModule, MatSidenavModule, MatButtonModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 
 export class SidebarComponent {
-  @Input() isExpanded: boolean = true;
+  @Input() isExpanded: boolean = false;
 
   actionSidebar(){
-      !this.isExpanded;
+    const sidebar = document.querySelector(".app-sidenav");
+    sidebar?.classList.toggle('hidde-sidebar');
+    this.isExpanded = !this.isExpanded;
   }
 }
